ed8ba559bb8d5bc80101f3aa0484ae0088b21b8c
Jamie Byram <jamie.byram@gmail.com>
2018-02-18 21:55:27 +0000

    Add newGrid function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 // Global variables
 
-// Create initial grid
+const container = document.querySelector('#container');
+
+// Function to create a new grid
+
+function newGrid(squaresPerSide) {
+  for (i = 0; i < squaresPerSide * squaresPerSide; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+  }
+}
+
+// Initial 16x16 grid
+
+newGrid(16);
 
 
 
