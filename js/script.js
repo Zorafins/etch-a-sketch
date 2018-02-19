f652cce49906a9bb5d8a3d1d00955813edbe37b2
Jamie Byram <jamie.byram@gmail.com>
2018-02-19 19:59:57 +0000

    Add event listeners

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // Global variables
 
 const container = document.querySelector('#container');
+const newgrid = document.querySelector('#newgrid')
 
 // Function to create a new grid
 
-function newGrid(squaresPerSide) {
+function createGrid(squaresPerSide) {
   for (i = 0; i < squaresPerSide * squaresPerSide; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
@@ -12,9 +13,25 @@ function newGrid(squaresPerSide) {
   }
 }
 
+// Event listener for button
+
+newgrid.addEventListener("click", function(){
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  let squaresPerSide = prompt("Please enter how many squares per side");
+  createGrid(squaresPerSide);
+})
+
+// Event Listener for each div 
+
+container.addEventListener("mouseover", function(e){
+  if (e.target !== e.currentTarget) {
+    e.target.style.background = 'black';
+  }
+  e.stopPropagation();
+})
+
 // Initial 16x16 grid
 
-newGrid(16);
-
-
-
+createGrid(16);
